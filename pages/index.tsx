328c5dc54cdb6f4f3de70872f271f8b6d8b60b23
Petra Jaros <peeja@peeja.com>
2021-11-03 10:45:05 -0400

    Show all tasks which are incomplete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
 import { useRead } from "../hooks/useRead";
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
-import { Construct } from "@m-ld/m-ld";
+import { Describe } from "@m-ld/m-ld";
 
 // react-json-view can't load in SSR
 const ReactJson = dynamic(() => import("react-json-view"), {
   ssr: false,
 });
 
-// This is a dummy query for now, chosen to demonstrate that updates come
-// through, without pulling too much data (eg, describing all entities), which
-// currently times out in m-ld.
-const QUERY: Construct = {
-  "@construct": { "@id": "?id", name: "?name" },
+// Show all tasks which are incomplete.
+const QUERY: Describe = {
+  "@describe": "?id",
   "@where": {
     "@id": "?id",
-    "http://peeja.com/purl/matters/omnifocus/o/name": "?name",
+    "http://peeja.com/purl/matters/omnifocus/o/completed": {
+      "@id": "http://peeja.com/purl/matters/omnifocus/o/never",
+    },
   },
 };
 
